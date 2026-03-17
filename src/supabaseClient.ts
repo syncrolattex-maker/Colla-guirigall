545a084e1526af5ce6b8bc0bddf283d503dc5340
aaronens3 <aarprialm@alu.edu.gva.es>
2026-03-17 12:21:17 +0100

    fix: revert to implicit auth flow to resolve login redirect loop

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -12,8 +12,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
-    flowType: 'pkce'
+    detectSessionInUrl: true
   },
   realtime: {
     params: {
